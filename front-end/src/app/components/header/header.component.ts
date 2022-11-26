7335623569ff35e8b5c335a8bf5fad4319b38e46
Oleh <oleg_bob@ukr.net>
2022-11-26 15:53:46 +0200

    created header, footer, base nave menu

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  collapsed: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLogout() {
+
+  }
 }
