4fb6ab753dea828e6715940b9ffe7d7b819b0646
Oleh <oleg_bob@ukr.net>
2022-11-28 19:51:37 +0200

    imports added

--- front-end/src/app/pages/home/home.module.ts
+++ front-end/src/app/pages/home/home.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SharedModule} from "../../shared/shared.module";
+import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home.component";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild([{path: '', component: HomeComponent}])
   ]
 })
 export class HomeModule { }
--- front-end/src/app/pages/info/info.module.ts
+++ front-end/src/app/pages/info/info.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SharedModule} from "../../shared/shared.module";
+import {RouterModule} from "@angular/router";
+import {InfoComponent} from "./info.component";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild([{path: '', component: InfoComponent}])
   ]
 })
 export class InfoModule { }
