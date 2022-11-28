58727e4e8abc4740cc111c4a1c45f2105aa50e59
Oleh <oleg_bob@ukr.net>
2022-11-28 19:54:41 +0200

    mat modules deleted

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,17 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatDividerModule} from "@angular/material/divider";
-import { LoginComponent } from './pages/auth/login/login.component';
 import {SharedModule} from "./shared/shared.module";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatSelectModule} from "@angular/material/select";
-import {MatInputModule} from "@angular/material/input";
-import { RegisterComponent } from './pages/auth/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InfoComponent } from './pages/info/info.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
 
 @NgModule({
@@ -24,22 +17,16 @@ import { RegisterComponent } from './pages/auth/register/register.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    RegisterComponent
+    HomeComponent,
+    InfoComponent,
+    ShopComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatDividerModule,
     SharedModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
