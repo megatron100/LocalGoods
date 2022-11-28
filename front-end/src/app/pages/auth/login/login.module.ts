1bc22007c39fd25b5644679460dd4a91fe418f00
Oleh <oleg_bob@ukr.net>
2022-11-28 19:50:12 +0200

    imports added

--- front-end/src/app/pages/auth/login/login.module.ts
+++ front-end/src/app/pages/auth/login/login.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {LoginComponent} from "./login.component";
+import {SharedModule} from "../../../shared/shared.module";
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild([{path: '', component: LoginComponent}])
   ]
 })
 export class LoginModule { }
