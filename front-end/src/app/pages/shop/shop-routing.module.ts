1a09753478225c0ec2a33d4d8fc33c0f925089bd
Oleh <oleg_bob@ukr.net>
2022-11-28 19:44:32 +0200

    routes added

--- front-end/src/app/pages/shop/shop-routing.module.ts
+++ front-end/src/app/pages/shop/shop-routing.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {ShopComponent} from "./shop.component";
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ShopComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
-export class ShopRoutingModule { }
+export class ShopRoutingModule {
+}
