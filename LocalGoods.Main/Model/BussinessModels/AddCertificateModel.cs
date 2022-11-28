01ab415b47c0fa577ee693f8a7766e563aba0019
Saurabh Singh <Saurabh_Singh@epam.com>
2022-11-28 16:32:51 +0530

    Bussinessmodal added

---
+++ LocalGoods.Main/Model/BussinessModels/AddCertificateModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalGoods.Main.Model.BussinessModels
+{
+    public class AddCertificateModel
+    {
+        [Required]
+        public string QualityCertificateTitle { get; set; }
+        [Required]
+        public string? QualityCertificateDescription { get; set; }
+        [Required]
+        public string? QualityCertificateLink { get; set; }
+        [Required]
+        public string? QualityCertificateDeleteLink { get; set; }
+        [Required]
+        public string TaxNumber { get; set; }
+    }
+}
