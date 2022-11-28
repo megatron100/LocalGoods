227395dbe1e2c306c19b28a9acf7b7b8995c675e
Shubham Maurya <Shubham_Maurya@epam.com>
2022-11-29 05:25:30 +0530

    ignored files

---
+++ LocalGoods.Main/Model/BussinessModels/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalGoods.Main.Model.BussinessModels
+{
+    public class Role
+    {
+        public static string Customer = "customer";
+        public static string Seller = "seller";
+    }
+}
