7c607134195e7036764307b20b0ed3d229c6d515
Oleh <oleg_bob@ukr.net>
2022-11-28 19:34:35 +0200

    shop module created

--- front-end/src/app/pages/shop/shop.module.ts
+++ front-end/src/app/pages/shop/shop.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SharedModule} from "../../shared/shared.module";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    SharedModule,
+    CommonModule,
+
   ]
 })
 export class ShopModule { }
