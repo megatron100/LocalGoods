92f47b7a8d2adac580d26316c37f7a7d6ce07472
Oleh <oleg_bob@ukr.net>
2022-11-28 19:48:06 +0200

    FormControl value replaced on null

--- front-end/src/app/pages/auth/register/register.component.ts
+++ front-end/src/app/pages/auth/register/register.component.ts
@@ -15,18 +15,18 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      'nickName': new FormControl('oleg', [Validators.required, Validators.minLength(3)]),
-      'email': new FormControl('oleg_bob@ukr.net', [Validators.required, Validators.email, Validators.pattern(EMAIL_PATTERN)]),
-      'password': new FormControl('111111', [Validators.required, Validators.minLength(6)]),
-      'password-confirm': new FormControl('111111', [Validators.required, Validators.minLength(6)]),
-      'role': new FormControl('seller', [Validators.required])
+      'nickName': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      'email': new FormControl(null, [Validators.required, Validators.email, Validators.pattern(EMAIL_PATTERN)]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'password-confirm': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'role': new FormControl(null, [Validators.required])
     })
   }
 
   onSubmit() {
-    // if (!this.registerForm.valid) {
-    //   return
-    // }
+    if (!this.registerForm.valid) {
+      return
+    }
     console.log(this.registerForm)
 
   }
