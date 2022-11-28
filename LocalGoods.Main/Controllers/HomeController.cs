e589bd73478adef552e4e02b808e9fe4e1c2dfe3
Shubham Maurya <Shubham_Maurya@epam.com>
2022-11-28 16:42:02 +0530

    commit staged

---
+++ LocalGoods.Main/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using LocalGoods.Main.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LocalGoods.Main.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles ="customer")]
+    public class HomeController : ControllerBase
+    {
+        private readonly LocalGoodsDbContext _dbContext;
+        
+        public HomeController(LocalGoodsDbContext dbContext   )
+        {
+            _dbContext = dbContext;
+             
+        }
+        [HttpGet("GetProducts")]
+        public async Task<IActionResult> GetProducts()
+        {
+            
+            return Ok();
+
+        }
+    }
+}
---
+++ LocalGoods.Main/Model/BussinessModels/AddressRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalGoods.Main.Model.BussinessModels
+{
+    public class AddressRequest
+    {
+        [Required]
+        public string PinCode { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Area { get; set; }
+        
+        public string? Cordinates { get; set; }
+    }
+}
