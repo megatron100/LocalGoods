db07014d443ce0c82f37ce359b4eae96b58cc15c
Oleh <oleg_bob@ukr.net>
2022-11-28 19:54:18 +0200

    routes added, lazy loading implemented

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', loadChildren: () => import('./pages/auth/login/login.module')
+      .then(m => m.LoginModule)
+  },
+  {path: 'register', loadChildren: () => import('./pages/auth/register/register.module')
+      .then(m => m.RegisterModule)
+  },
+  {path: 'home', loadChildren: () => import('./pages/home/home.module')
+      .then(m => m.HomeModule)
+  },
+  {path: 'info', loadChildren: () => import('./pages/info/info.module')
+      .then(m => m.InfoModule)
+  },
+  {path: 'contacts', loadChildren: () => import('./pages/contacts/contacts.module')
+      .then(m => m.ContactsModule)
+  },
+  {path: 'shop', loadChildren: () => import('./pages/shop/shop.module')
+      .then(m => m.ShopModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
