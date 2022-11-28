f4a9979cfb88e827e933c769f996152f1b7b4528
Oleh <oleg_bob@ukr.net>
2022-11-28 18:52:02 +0200

    login page done

--- front-end/src/app/pages/auth/login/login.component.ts
+++ front-end/src/app/pages/auth/login/login.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {EMAIL_PATTERN} from "../../../constants/constants";
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
   loginForm: FormGroup = new FormGroup({
     'email': new FormControl(null, [Validators.required, Validators.pattern(EMAIL_PATTERN)]),
     'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
