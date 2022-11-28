e26673e511c6128f6cbf4eaa5d8e1a460f43b64b
Oleh <oleg_bob@ukr.net>
2022-11-28 18:48:34 +0200

    register component done

--- front-end/src/app/pages/auth/register/register.component.ts
+++ front-end/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {EMAIL_PATTERN} from "../../../constants/constants";
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm!: FormGroup;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      'nickName': new FormControl('oleg', [Validators.required, Validators.minLength(3)]),
+      'email': new FormControl('oleg_bob@ukr.net', [Validators.required, Validators.email, Validators.pattern(EMAIL_PATTERN)]),
+      'password': new FormControl('111111', [Validators.required, Validators.minLength(6)]),
+      'password-confirm': new FormControl('111111', [Validators.required, Validators.minLength(6)]),
+      'role': new FormControl('seller', [Validators.required])
+    })
   }
 
+  onSubmit() {
+    // if (!this.registerForm.valid) {
+    //   return
+    // }
+    console.log(this.registerForm)
+
+  }
 }
