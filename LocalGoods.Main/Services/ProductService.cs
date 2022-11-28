9e76469b3ded9cdbc2cb1e2b3501cbfa2169e391
Saurabh Singh <Saurabh_Singh@epam.com>
2022-11-28 13:51:23 +0530

    service staged

---
+++ LocalGoods.Main/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using LocalGoods.Main.DAL;
+using LocalGoods.Main.Model;
+
+namespace LocalGoods.Main.Services
+{
+    public class ProductService
+    {
+        private readonly LocalGoodsDbContext _dbContext;
+        public ProductService(LocalGoodsDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public Product GetProductById(int id)
+        {
+            if (id == 0)
+            {
+                return null;
+            }
+            Product product = _dbContext.Product.Where(x => x.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return null;
+            }
+            return product;
+
+        }
+
+    }
+}
