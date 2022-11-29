8e239430bba71e72fa252095bea86b9091fa323a
Oleh <oleg_bob@ukr.net>
2022-11-29 16:08:08 +0200

    component styled? base logic added

--- front-end/src/app/components/sub-menu-header/sub-menu-header.component.ts
+++ front-end/src/app/components/sub-menu-header/sub-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PRODUCT_SORT_VALUES} from "../../constants/constants";
 
 @Component({
   selector: 'app-sub-menu-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubMenuHeaderComponent implements OnInit {
 
+  sortValues: string[] = PRODUCT_SORT_VALUES;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sortValueChange($event: any) {
+    console.log($event.target.value)
+  }
+
+  searchChange($event: any) {
+    console.log($event.target.value)
+  }
 }
