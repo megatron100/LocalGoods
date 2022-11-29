429570745ff0244510133b560597cf8b2a71d4b2
Oleh <oleg_bob@ukr.net>
2022-11-29 15:13:34 +0200

    sub-menu-header.component created

---
+++ front-end/src/app/components/sub-menu-header/sub-menu-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-menu-header',
+  templateUrl: './sub-menu-header.component.html',
+  styleUrls: ['./sub-menu-header.component.scss']
+})
+export class SubMenuHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
