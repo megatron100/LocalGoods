733a2c449abadddb33fca5a67163ec8abb27e5ea
Saurabh Singh <Saurabh_Singh@epam.com>
2022-11-29 14:02:02 +0530

    stage changes

---
+++ LocalGoods.Main/Model/OrderModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LocalGoods.Main.Model
+{
+    public class Order
+    {
+        [ForeignKey("OrderItem")]
+        public int OrderId { get; set; }
+        public DateTime OrderDate  { get; set; }
+        public string OrderName { get; set; }
+        public string PaymentType { get; set; }
+        public string OrderStatus { get; set; }
+        [ForeignKey("Address")]
+        public int AdressId { get; set; }
+
+        [ForeignKey("User")]
+        public int userId { get; set; }
+    }
+}
