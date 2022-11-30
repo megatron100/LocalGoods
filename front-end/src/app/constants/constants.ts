b67f8fbe94463896858a5773bd22bd40a99ccba6
Oleh <oleg_bob@ukr.net>
2022-11-30 13:33:46 +0200

    constants added

--- front-end/src/app/constants/constants.ts
+++ front-end/src/app/constants/constants.ts
@@ -1,2 +1,6 @@
 export const EMAIL_PATTERN = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$';
 export const PRODUCT_SORT_VALUES = ['ASC Name', 'DESC Name', 'ASC Price', 'DSC Price'];
+export const API = 'http://';
+export const API_PATH_AUTH = 'api/auth';
+export const PATH_LOGIN = 'login';
+export const PATH_REGISTER = 'register';
