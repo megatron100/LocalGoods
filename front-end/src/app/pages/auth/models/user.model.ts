5071c3fc46b7ee0b2c404531d66c23b56f635d2e
Oleh <oleg_bob@ukr.net>
2022-11-30 13:21:52 +0200

    user model created

--- front-end/src/app/pages/auth/models/user.model.ts
+++ front-end/src/app/pages/auth/models/user.model.ts
@@ -1,16 +1,37 @@
+import {UserPaymentCardModel} from "./user-payment-card.model";
+
 export class User {
   constructor(
-    public email: string,
-    public id: string,
+    public data: {
+        address: {
+          pinCode: string,
+          country: string,
+          city: string,
+          area: string,
+          coordinates: string,
+          createdDate: string
+        },
+      cardList: UserPaymentCardModel[],
+      certification:[],
+      email: string,
+      name: string,
+      mobile: string,
+      role: string,
+      sellerRating: number
+    },
     private _token: string,
+    private _refresh_token: string,
     private _tokenExpirationDate: Date,
+    private _refreshTokenExpirationDate: Date
   ) {
   }
 
 //if the token is valid, return it if not, null
   get token() {
-    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+    if (!this._tokenExpirationDate || new Date() > this._refreshTokenExpirationDate) {
       return null
+    } else if (new Date() > this._tokenExpirationDate && new Date() < this._refreshTokenExpirationDate) {
+      return this._refresh_token
     }
     return this._token
   }
