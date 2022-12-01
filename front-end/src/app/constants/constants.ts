b0f3548670df9c742bcfc8f8aacb40910639982b
Oleh <oleg_bob@ukr.net>
2022-12-01 19:40:19 +0200

    USER_ROLES added

--- front-end/src/app/constants/constants.ts
+++ front-end/src/app/constants/constants.ts
@@ -1,8 +1,9 @@
 export const EMAIL_PATTERN = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$';
 export const PRODUCT_SORT_VALUES = ['ASC Name', 'DESC Name', 'ASC Price', 'DSC Price'];
-export const API = 'http://';
-export const API_PATH_AUTH = 'api/auth';
-export const PATH_LOGIN = 'login';
-export const PATH_REGISTER = 'register';
+export const API = 'https://finallocalgoods.azurewebsites.net';
+export const API_PATH_AUTH = 'Auth';
+export const PATH_LOGIN = 'Login';
+export const PATH_REGISTER = 'Registration';
 export const EXPIRE_IN = 1440;
 export const REFRESH_EXPIRE_IN = 14400;
+export const USER_ROLES = ['seller', 'customer'];
