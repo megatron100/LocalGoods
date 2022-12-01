051e4d1ddd6b6d69080ca298c911f0bd082ede65
Oleh <oleg_bob@ukr.net>
2022-12-01 19:50:45 +0200

    some changes

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./pages/auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit{
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit() {
+    this.authService.autoLogin()
+  }
 }
