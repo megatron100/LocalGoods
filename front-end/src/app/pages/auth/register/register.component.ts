c89ea132f83c50b6c33c5b85794c71f9bc880bb2
Oleh <oleg_bob@ukr.net>
2022-12-01 19:38:16 +0200

    some formControlNames renamed

--- front-end/src/app/pages/auth/register/register.component.ts
+++ front-end/src/app/pages/auth/register/register.component.ts
@@ -16,10 +16,10 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      'nickName': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      'name': new FormControl(null, [Validators.required, Validators.minLength(3)]),
       'email': new FormControl(null, [Validators.required, Validators.email, Validators.pattern(EMAIL_PATTERN)]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      'password-confirm': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'rePassword': new FormControl(null, [Validators.required, Validators.minLength(6)]),
       'role': new FormControl(null, [Validators.required])
     })
   }
