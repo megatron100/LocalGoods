8a33817fedded88748da12d9ddb89281e047f934
Oleh <oleg_bob@ukr.net>
2022-12-01 19:47:06 +0200

    added role checking

--- front-end/src/app/pages/auth/auth.guard.ts
+++ front-end/src/app/pages/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
-import {map, Observable, take} from 'rxjs';
+import {Observable} from 'rxjs';
 import {AuthService} from "./auth.service";
 
 @Injectable({
@@ -14,16 +14,16 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.user
-      .pipe(
-        take(1),
-        map(user => {
-          const isAuth = !!user;
-          if (isAuth) {
-            return true
-          }
-          return this.router.createUrlTree(['/login'])
-        })
-      );
+    return this.checkUserLogin(route);
+  }
+
+  checkUserLogin(route: ActivatedRouteSnapshot): boolean {
+      const userRole = this.authService.getRole();
+    console.log(userRole)
+      if (route.data['role'] && route.data['role'].indexOf(userRole) === -1) {
+        this.router.navigate(['/home']);
+        return false;
+      }
+      return true;
   }
 }
