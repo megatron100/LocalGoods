fb7e6ff872d56d9a3b624e7fcf835b1893e823a1
Oleh <oleg_bob@ukr.net>
2022-12-01 19:47:30 +0200

    obj modified

--- front-end/src/app/interfaces/auth-response-data.ts
+++ front-end/src/app/interfaces/auth-response-data.ts
@@ -1,8 +1,10 @@
 export interface AuthResponseData {
-  userId: string
-  userEmail: string
-  role: string
-  nickName: string
-  accessToken: string
-  refreshToken: string
+ data: {
+   userId: string
+   userEmail: string
+   role: string
+   nickName: string
+   accessToken: string
+   refreshToken: string
+ }
 }
