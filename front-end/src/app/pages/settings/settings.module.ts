8d128dd5fc59b7f9ea9d7849ef6a636e3d095fc1
Oleh <oleg_bob@ukr.net>
2022-12-02 19:12:13 +0200

    UserDataUpdateDialogComponent added to declarations

--- front-end/src/app/pages/settings/settings.module.ts
+++ front-end/src/app/pages/settings/settings.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SettingsRoutingModule} from "./settings-routing.module";
 import {SharedModule} from "../../shared/shared.module";
+import { UserDataUpdateDialogComponent } from './user-data-update-dialog/user-data-update-dialog.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDataUpdateDialogComponent
+  ],
   imports: [
     SharedModule,
     CommonModule,
