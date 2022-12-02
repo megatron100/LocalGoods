08fa0d329257c9192d75dddc4fc04ee7f327c92b
Oleh <oleg_bob@ukr.net>
2022-12-02 19:11:06 +0200

    user-data-update-dialog.component created

---
+++ front-end/src/app/pages/settings/user-data-update-dialog/user-data-update-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-data-update-dialog',
+  templateUrl: './user-data-update-dialog.component.html',
+  styleUrls: ['./user-data-update-dialog.component.scss']
+})
+export class UserDataUpdateDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
