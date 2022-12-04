afa045d188743e2ea09b64e22642cc4adc1e8e14
Oleh <oleg_bob@ukr.net>
2022-12-04 13:45:34 +0200

    user-data-update-dialog.component updated

--- front-end/src/app/pages/settings/user-data-update-dialog/user-data-update-dialog.component.ts
+++ front-end/src/app/pages/settings/user-data-update-dialog/user-data-update-dialog.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import { ViewChild, ElementRef } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-user-data-update-dialog',
@@ -9,60 +8,32 @@ import { ViewChild, ElementRef } from '@angular/core';
 })
 export class UserDataUpdateDialogComponent implements OnInit {
 
-  updateUserForm!: FormGroup
-  inputType: string[] = ['text', 'number'];
-  inputName: string[] = ['name', 'email', 'mobile', 'cardNumber', 'postCode', 'country', 'city', 'area'];
-  @ViewChild('ccNumber') ccNumberField!: ElementRef;
+  userForm!: FormGroup;
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
-    this.updateUserForm = new FormGroup({
-      'name': new FormControl(''),
-      'email': new FormControl(''),
-      'mobile': new FormControl(''),
-      'cardNumber': new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(17)]),
-      'postCode': new FormControl(''),
-      'country': new FormControl(''),
-      'city': new FormControl(''),
-      'area': new FormControl(''),
-    })
+    this.userForm = this.fb.group({
+      basicInfo: this.fb.group({
+        name: [],
+        email: [],
+        mobile: [],
+      }),
+      address: this.fb.group({
+        postCode: [],
+        country: [],
+        city: [],
+        area: []
+      })
+    });
+
   }
+
 
   onSubmit() {
-
+    console.log(this.userForm.value)
   }
 
-  creditCardNumberSpacing() {
-    const input = this.ccNumberField.nativeElement;
-    const { selectionStart } = input;
-
-    const { cardNumber } = this.updateUserForm.controls;
-
-    let trimmedCardNum = cardNumber.value.replace(/\s+/g, '');
-    console.log(trimmedCardNum)
-    if (trimmedCardNum.length > 16) {
-      trimmedCardNum = trimmedCardNum.substr(0, 16);
-    }
-
-    /* Handle American Express 4-6-5 spacing */
-    const partitions = trimmedCardNum.startsWith('34') || trimmedCardNum.startsWith('37')
-      ? [4,6,5]
-      : [4,4,4,4];
-
-    const numbers: number[] = [];
-    let position = 0;
-    partitions.forEach(partition => {
-      const part = trimmedCardNum.substr(position, partition);
-      if (part) numbers.push(part);
-      position += partition;
-    })
-
-    cardNumber.setValue(numbers.join(' '));
-
-    /* Handle caret position if user edits the number later */
-    if (selectionStart < cardNumber.value.length - 1) {
-      input.setSelectionRange(selectionStart, selectionStart, 'none');
-    }
-  }
 }
