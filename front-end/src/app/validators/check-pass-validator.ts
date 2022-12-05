9ec0785362cfaca6aab3927b29998ed6875b99ac
Oleh <oleg_bob@ukr.net>
2022-12-05 21:02:57 +0200

    bug fixed

--- front-end/src/app/validators/check-pass-validator.ts
+++ front-end/src/app/validators/check-pass-validator.ts
@@ -4,8 +4,8 @@ import {Injectable} from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class CustomValidators {
   static checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => {
-    let pass = group.get('password')?.value;
-    let confirmPass = group.get('confirmPassword')?.value
+    let pass = group.get('newPassword')?.value;
+    let confirmPass = group.get('passConfirm')?.value
     return pass === confirmPass ? null : { notSame: true }
   }
 }
