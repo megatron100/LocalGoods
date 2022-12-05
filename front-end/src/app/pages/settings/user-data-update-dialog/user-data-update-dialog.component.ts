654f1b55a2d40d5076e9f291c7e28b6c0a12fff8
Oleh <oleg_bob@ukr.net>
2022-12-05 20:58:56 +0200

    update user data logic added

--- front-end/src/app/pages/settings/user-data-update-dialog/user-data-update-dialog.component.ts
+++ front-end/src/app/pages/settings/user-data-update-dialog/user-data-update-dialog.component.ts
@@ -1,39 +1,85 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {SettingsService} from "../../../services/settings.service";
+import {EMAIL_PATTERN} from "../../../constants/constants";
+import {Subscription} from "rxjs";
+import {AuthService} from "../../auth/auth.service";
 
 @Component({
   selector: 'app-user-data-update-dialog',
   templateUrl: './user-data-update-dialog.component.html',
   styleUrls: ['./user-data-update-dialog.component.scss']
 })
-export class UserDataUpdateDialogComponent implements OnInit {
+export class UserDataUpdateDialogComponent implements OnInit, OnDestroy {
 
   userForm!: FormGroup;
+  private subscription!: Subscription;
 
-
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private settingsService: SettingsService, private authService: AuthService) {
   }
 
   ngOnInit(): void {
-    this.userForm = this.fb.group({
-      basicInfo: this.fb.group({
-        name: [],
-        email: [],
-        mobile: [],
+    this.initForm()
+  }
+
+  private initForm() {
+    let name = '';
+    let email = '';
+    let mobile = '';
+    let postCode = '';
+    let country = '';
+    let city = '';
+    let area = '';
+
+    this.userForm = new FormGroup({
+      basicInfo: new FormGroup({
+        name: new FormControl(null, []),
+        email: new FormControl(null, [Validators.email, Validators.pattern(EMAIL_PATTERN)]),
+        mobile: new FormControl(null, []),
       }),
-      address: this.fb.group({
-        postCode: [],
-        country: [],
-        city: [],
-        area: []
+      address: new FormGroup({
+        postCode: new FormControl(null, []),
+        country: new FormControl(null, []),
+        city: new FormControl(null, []),
+        area: new FormControl(null, []),
       })
     });
 
+    this.subscription = this.authService.user
+      .subscribe(user => {
+        if(user) {
+          name = user.nickName;
+          email = user.email;
+          mobile = user.mobile || '';
+          postCode = user.address?.pinCode || '';
+          country = user.address?.country || ''
+          city = user.address?.city || ''
+          area = user.address?.area || ''
+        }
+      })
+
+    this.userForm = new FormGroup({
+      basicInfo: new FormGroup({
+        name: new FormControl(name, [Validators.required, Validators.minLength(3)]),
+        email: new FormControl(email, [Validators.required, Validators.pattern(EMAIL_PATTERN)]),
+        mobile: new FormControl(mobile, []),
+      }),
+      address: new FormGroup({
+        postCode: new FormControl(postCode, []),
+        country: new FormControl(country, []),
+        city: new FormControl(city, []),
+        area: new FormControl(area, []),
+      })
+    })
   }
 
 
   onSubmit() {
-    console.log(this.userForm.value)
+    this.settingsService.updateUserInfo(this.userForm.value)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 
 }
