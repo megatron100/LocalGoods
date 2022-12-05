029ad9dab39cd60dcbffa0a393b7270da07db59f
Oleh <oleg_bob@ukr.net>
2022-12-05 10:30:41 +0200

    BasicInfoComponent,    AddressComponent components added

--- front-end/src/app/pages/settings/settings.module.ts
+++ front-end/src/app/pages/settings/settings.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import {SettingsRoutingModule} from "./settings-routing.module";
 import {SharedModule} from "../../shared/shared.module";
 import { UserDataUpdateDialogComponent } from './user-data-update-dialog/user-data-update-dialog.component';
-
+import { BasicInfoComponent } from './user-data-update-dialog/basic-info/basic-info.component';
+import { AddressComponent } from './user-data-update-dialog/address/address.component';
 
 
 @NgModule({
   declarations: [
-    UserDataUpdateDialogComponent
+    UserDataUpdateDialogComponent,
+    BasicInfoComponent,
+    AddressComponent,
   ],
   imports: [
     SharedModule,
