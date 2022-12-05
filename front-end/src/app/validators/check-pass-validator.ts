abe873ece40c9a086ccbf9cc8321d91fc6d3f6d5
Oleh <oleg_bob@ukr.net>
2022-12-05 17:42:31 +0200

    validator created

---
+++ front-end/src/app/validators/check-pass-validator.ts
@@ -0,0 +1,11 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+import {Injectable} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class CustomValidators {
+  static checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => {
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value
+    return pass === confirmPass ? null : { notSame: true }
+  }
+}
