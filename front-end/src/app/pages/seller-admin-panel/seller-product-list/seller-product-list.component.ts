95b8d0d7a582b6e52fed44a419bc6c76bc0a617e
Oleh <oleg_bob@ukr.net>
2022-12-06 16:48:27 +0200

    seller product-list added

---
+++ front-end/src/app/pages/seller-admin-panel/seller-product-list/seller-product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-product-list',
+  templateUrl: './seller-product-list.component.html',
+  styleUrls: ['./seller-product-list.component.scss']
+})
+export class SellerProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
