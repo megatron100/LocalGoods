f6b7c1427049fb2e4b2dddf83e11f0207748f125
Oleh <oleg_bob@ukr.net>
2022-12-08 10:27:26 +0200

    sellerProductDetail comp created

---
+++ front-end/src/app/pages/seller-admin-panel/seller-product-list/seller-product-details/seller-product-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-product-details',
+  templateUrl: './seller-product-details.component.html',
+  styleUrls: ['./seller-product-details.component.scss']
+})
+export class SellerProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
