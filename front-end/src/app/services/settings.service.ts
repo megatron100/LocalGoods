542239dbfefe6e322d06a53ca43c1bf48f3a0e11
Oleh Babiak <66703682+OlehBabiak@users.noreply.github.com>
2022-12-12 17:04:40 +0200

    Wip user settings update (#20)
    * warning fixed
    
    * added loader and updated component with async pipe
    
    * deleted card list row, added ngIf in to certification row
    
    * bug fixed
    
    * updated page with table
    
    * comments deleted
    
    * bug fixed
    
    * adding certificate implemented
    
    * button styled
    
    * certification add implemented
    
    * bag fixed
    
    * bag fixed

--- front-end/src/app/services/settings.service.ts
+++ front-end/src/app/services/settings.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {API, API_PATH, PATH_CHANGE_PASS, PATH_EDIT} from "../constants/constants";
-import {tap} from "rxjs";
+import {
+  API,
+  API_PATH,
+  API_PATH_SELLER,
+  PATH_ADD_CERTIFICATE,
+  PATH_CHANGE_PASS,
+  PATH_EDIT
+} from "../constants/constants";
 import {UserUpdateResponseData} from "../interfaces/userUpdateResponseData";
 
 @Injectable({
@@ -14,11 +20,7 @@ export class SettingsService {
   updateUserInfo(body: {}) {
     return this.http.put<UserUpdateResponseData>(`${API}${API_PATH}/${PATH_EDIT}`, body)
       .pipe(
-        tap(
-          ({data}) => {
 
-          }
-        )
       )
   };
 
@@ -26,6 +28,13 @@ export class SettingsService {
     return this.http.put<any>(`${API}${API_PATH}/${PATH_CHANGE_PASS}`, body)
       .pipe(
 
+      )
+  };
+
+  addCertificate(body: {}) {
+    console.log(body)
+    return this.http.post<any>(`${API}/${API_PATH_SELLER}/${PATH_ADD_CERTIFICATE}`, body)
+      .pipe(
       )
   };
 }
