d737ede3788d38c167ee1d3e372c9012f3d8feba
Saurabh Singh <Saurabh_Singh@epam.com>
2022-12-12 14:24:07 +0530

    seller confirm order

--- LocalGoods.Main/Model/BussinessModels/OrderStatus.cs
+++ LocalGoods.Main/Model/BussinessModels/OrderStatus.cs
@@ -2,8 +2,8 @@
 {
     public static class OrderStatus
     {
-        public const string Pending = "pending";
-        public const string Delivered = "delivered";
+        public const string Pending = "Pending";
+        public const string Confirmed = "Confirmed";
         public const string Refused = "refused";
         public const string Refunded = "refunded";
     }
