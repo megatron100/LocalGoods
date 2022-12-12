e22c31407085ebc022e0344b52594e08ff003abb
Oleh Babiak <66703682+OlehBabiak@users.noreply.github.com>
2022-12-12 23:31:24 +0200

    Wip info contacts (#24)
    * warning fixed
    
    * added loader and updated component with async pipe
    
    * deleted card list row, added ngIf in to certification row
    
    * bug fixed
    
    * updated page with table
    
    * comments deleted
    
    * bug fixed
    
    * adding certificate implemented
    
    * button styled
    
    * certification add implemented
    
    * bag fixed
    
    * bag fixed
    
    * info added
    
    * contacts added
    
    * bug fixed

--- front-end/src/app/shared/dialogs/message-dialog/message-dialog.component.ts
+++ front-end/src/app/shared/dialogs/message-dialog/message-dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-message-dialog',
@@ -7,9 +7,12 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
   styleUrls: ['./message-dialog.component.scss']
 })
 export class MessageDialogComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
-
-  ngOnInit(): void {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<MessageDialogComponent>) {
   }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.dialogRef.close()
+    }, 2000)
+  }
 }
