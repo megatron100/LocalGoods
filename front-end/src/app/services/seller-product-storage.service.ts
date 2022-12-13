9ff49a97bfc69070067bb4b0c9ba380655d8ff3e
Oleh Babiak <66703682+OlehBabiak@users.noreply.github.com>
2022-12-13 20:30:16 +0200

    Wip errors (#27)
    * bug fixed
    
    * Error handling implemented

--- front-end/src/app/services/seller-product-storage.service.ts
+++ front-end/src/app/services/seller-product-storage.service.ts
@@ -7,24 +7,28 @@ import {
   PATH_EDIT_PRODUCT_BY_ID, PATH_GET_CATEGORIES,
   PATH_GET_PRODUCT_BY_ID,
   PATH_GET_PRODUCTS,
-  Local_API,
 } from "../constants/constants";
 import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs";
+import {catchError, map} from "rxjs";
 import {UserService} from "./user.service";
 import {SellerProductItemModel} from "../pages/seller-admin-panel/models/seller-product-item.model";
+import {ErrorService} from "../shared/error-handling/error.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerProductStorageService {
 
-  constructor(private http: HttpClient, private userService: UserService) {
+  constructor(
+    private http: HttpClient,
+    private userService: UserService,
+    private errorService: ErrorService) {
   }
 
   storeProduct(product: SellerProductItemModel) {
     return this.http.post<any>(`${API}/${API_PATH_SELLER}/${PATH_ADD_PRODUCT}`, product)
       .pipe(
+        catchError(this.errorService.handleError),
         map(({data}) => {
           return this.userService.transformProductArrResponse(data)
         })
@@ -34,6 +38,7 @@ export class SellerProductStorageService {
   deleteProduct(id: string) {
     return this.http.delete<any>(`${API}/${API_PATH_SELLER}/${PATH_DELETE_PRODUCT_BY_ID}/${id}`)
       .pipe(
+        catchError(this.errorService.handleError),
         map(({data}) => {
           return this.userService.transformProductArrResponse(data)
         })
@@ -44,6 +49,7 @@ export class SellerProductStorageService {
     const body = {...product, productId: id}
     return this.http.put<any>(`${API}${API_PATH_SELLER}/${PATH_EDIT_PRODUCT_BY_ID}`, body)
       .pipe(
+        catchError(this.errorService.handleError),
         map(({data}) => {
           return this.userService.transformProductArrResponse(data)
         })
@@ -51,9 +57,9 @@ export class SellerProductStorageService {
   };
 
   getProductById(id: string) {
-    console.log('service id', id)
     return this.http.get<any>(`${API}/${API_PATH_SELLER}/${PATH_GET_PRODUCT_BY_ID}/${id}`)
       .pipe(
+        catchError(this.errorService.handleError),
         map(({data}) => {
           return this.userService.transformProductResponse(data)
         })
@@ -61,8 +67,9 @@ export class SellerProductStorageService {
   };
 
   getProducts() {
-    return this.http.get<any>(`${Local_API}${API_PATH_SELLER}/${PATH_GET_PRODUCTS}`)
+    return this.http.get<any>(`${API}${API_PATH_SELLER}/${PATH_GET_PRODUCTS}`)
       .pipe(
+        catchError(this.errorService.handleError),
         map(({data}) => {
           return this.userService.transformProductArrResponse(data)
         })
@@ -72,7 +79,7 @@ export class SellerProductStorageService {
   getCategories() {
     return this.http.get<any>(`${API}/${API_PATH}/${PATH_GET_CATEGORIES}`)
       .pipe(
-
+        catchError(this.errorService.handleError),
       )
   };
 }
--- front-end/src/app/services/settings.service.ts
+++ front-end/src/app/services/settings.service.ts
@@ -9,31 +9,35 @@ import {
   PATH_EDIT
 } from "../constants/constants";
 import {UserUpdateResponseData} from "../interfaces/userUpdateResponseData";
+import {ResponseData} from "../interfaces/responseData";
+import {catchError} from "rxjs";
+import {ErrorService} from "../shared/error-handling/error.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private errorService: ErrorService) { }
 
   updateUserInfo(body: {}) {
     return this.http.put<UserUpdateResponseData>(`${API}${API_PATH}/${PATH_EDIT}`, body)
       .pipe(
-
+        catchError(this.errorService.handleError),
       )
   };
 
   changePassword(body: {}) {
-    return this.http.put<any>(`${API}${API_PATH}/${PATH_CHANGE_PASS}`, body)
+    return this.http.put<ResponseData>(`${API}${API_PATH}/${PATH_CHANGE_PASS}`, body)
       .pipe(
-
+        catchError(this.errorService.handleError),
       )
   };
 
   addCertificate(body: {}) {
-    return this.http.post<any>(`${API}/${API_PATH_SELLER}/${PATH_ADD_CERTIFICATE}`, body)
+    return this.http.post<ResponseData>(`${API}/${API_PATH_SELLER}/${PATH_ADD_CERTIFICATE}`, body)
       .pipe(
+        catchError(this.errorService.handleError),
       )
   };
 }
