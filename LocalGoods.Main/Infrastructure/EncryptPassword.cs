887155d1a53ac4b6f0c521b838af87cd1f6941d3
Saurabh Singh <Saurabh_Singh@epam.com>
2022-12-15 14:51:03 +0530

    encrypt change

---
+++ LocalGoods.Main/Infrastructure/EncryptPassword.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+namespace LocalGoods.Main.Infrastructure
+{
+    public static class EncryptPassword
+    {
+        public static string CalculateSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] hashValue;
+            UTF8Encoding objUtf8 = new UTF8Encoding();
+            hashValue = sha256.ComputeHash(objUtf8.GetBytes(str));
+
+            return Convert.ToBase64String(hashValue);
+        }
+    }
+}
