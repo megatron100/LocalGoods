f9d8d649980ee1be70817d5d1388ecc8cc36bbf0
Oleh <oleg_bob@ukr.net>
2023-04-25 23:14:32 +0300

    code refactored on Contacts page

--- front-end/src/app/pages/contacts/contacts.component.ts
+++ front-end/src/app/pages/contacts/contacts.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
 })
-export class ContactsComponent {}
+export class ContactsComponent {
+  name1 = '';
+  msg1 = '';
+
+  handleSubmit() {
+    const whatsappMessage = `Hello, My name is ${this.name1}. Message: ${this.msg1}`;
+    const newMessage = window.encodeURIComponent(whatsappMessage);
+    window.open(`https://wa.me/+380936981408?text=${newMessage}`);
+  }
+
+  onMsgChange($event: Event) {
+    this.msg1 = ($event.target as HTMLInputElement).value;
+  }
+
+  onNameChange($event: Event) {
+    this.name1 = ($event.target as HTMLInputElement).value;
+  }
+}
